d6e5db109fc8fc66cbff1c408cbb56e520215905
Jul Jen <juliajen.prog@gmail.com>
2018-03-21 19:35:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,8 +13,8 @@ class Student
   end
 
 
-  def self.new_from_db
-    new_student = self.create(name:, grade:)
+  def self.new_from_db(row)
+    new_student = self.create(row)
     # new_student.id = row[0]
     # new_student.name = row[1]
     # new_student.grade = row[2]
