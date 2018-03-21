55af113fbf067172cd0d14c31755215891e0cc22
Jul Jen <juliajen.prog@gmail.com>
2018-03-21 19:10:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,6 +11,14 @@ class Student
     @grade = grade
   end
 
+  def self.new_from_db(row) #accepts a row from the database as an argument
+    new_student = self.new# creates a new 'Student object' given a row from the database
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
+
 
   def self.create_table #  creates a table and gives table column names that match the attributes of an individual instance of student
     sql = <<-SQL
